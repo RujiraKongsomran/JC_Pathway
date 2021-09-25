4e40d9bc7c823d58f7cff2b0a104da7e6d65f96a
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-09-25 22:22:09 +0700

    Add multiple texts

--- app/src/main/java/com/rujirakongsomran/jc_pathway/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/jc_pathway/MainActivity.kt
@@ -14,25 +14,24 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard("Android")
+            MessageCard(Message("Android", "Jetpack Compose"))
         }
     }
 }
 
-@Composable
-fun MessageCard(name: String) {
-    Text(text = "Hello $name!")
-}
+data class Message(val author: String, val body: String)
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MessageCard(msg: Message) {
+    Text(text = msg.author)
+    Text(text = msg.body)
 }
 
 @Preview
 @Composable
 fun PreviewMessageCard() {
-    JC_PathwayTheme {
-        MessageCard("Android")
-    }
+    MessageCard(
+        msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!")
+    )
 }
+
