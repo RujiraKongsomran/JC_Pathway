7bfd9e3510a9d3479995d30bec84bc4f5fe6dee2
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-09-27 10:44:12 +0700

    Shape

--- app/src/main/java/com/rujirakongsomran/jc_pathway/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/jc_pathway/MainActivity.kt
@@ -54,10 +54,14 @@ fun MessageCard(msg: Message) {
             )
             // Add a vertical space between the author and message texts
             Spacer(modifier = Modifier.height(4.dp))
-            Text(
-                text = msg.body,
-                style = MaterialTheme.typography.body2
-            )
+            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    style = MaterialTheme.typography.body2
+                )
+            }
+
         }
     }
 }
